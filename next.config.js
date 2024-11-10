3f9447530202ee8be9890622785f3e8eac690e85
davestanyer <46975581+davestanyer@users.noreply.github.com>
2024-11-10 22:08:55 +1300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   trailingSlash: true,
+  distDir: '.next',
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  }
 };
 
 module.exports = nextConfig;
